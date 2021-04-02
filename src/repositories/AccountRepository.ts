a9d6e339e11905af228026cf2c46d618b5de3284
ku6ryo <ku6ryo@gmail.com>
2021-04-03 01:54:46 +0900

    added faker

--- src/repositories/AccountRepository.ts
+++ src/repositories/AccountRepository.ts
@@ -1,12 +1,27 @@
-import Account from "./Account"
+import Account from "../models/Account.model"
+import fakeAccount from "../models/Account.faker"
+
+
+let accounts = Array(100).map(() => {
+  return fakeAccount()
+})
 
 export default class AccountRepository {
-  create(account: Account) {
+  create (account: Account) {
+    accounts.push(account)
   }
-  get(id: string) {
+  get (id: string) {
+    return accounts.find((account) => {
+      return id === account.getId()
+    }) || null
   }
-  update(account: Account) {
+  update (account: Account) {
+    this.delete(account.getId())
+    this.create(account)
   }
-  delete(id: string) {
+  delete (id: string) {
+    accounts = accounts.filter(account => {
+      return account.getId() !== id
+    })
   }
 }
