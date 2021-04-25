69eecd5811326058d5655f1db7e7687dec4852bf
ku6ryo <ku6ryo@gmail.com>
2021-04-26 01:28:33 +0900

    added next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
+import {
+  useSession, signIn, signOut
+} from "next-auth/client"
 
-export default HomePage
+export default function Component() {
+  const [ session, loading ] = useSession()
+  if (session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+  return <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
+}
