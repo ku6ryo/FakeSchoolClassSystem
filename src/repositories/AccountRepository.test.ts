2e38144645af7cf83c4554f1f1b7d569a98d05f4
ku6ryo <ku6ryo@gmail.com>
2021-04-25 22:18:56 +0900

    preparation to run multiple repo test

--- src/repositories/AccountRepository.test.ts
+++ src/repositories/AccountRepository.test.ts
@@ -5,11 +5,28 @@ import AccountType from "../models/AccountType"
 import Account from "../models/Account.model"
 import Name from "../models/Name.model"
 import faker from "faker"
+import { getConnection } from "typeorm"
+
+beforeAll(async () => {
+  await setUpDb()
+})
+
+afterEach(async () => {
+  const connection = getConnection()
+  const entities = connection.entityMetadatas;
+  for (const entity of entities) {
+    const repository = connection.getRepository(entity.name)
+    await repository.clear()
+  }
+})
+
+afterAll(async () => {
+  await getConnection().close()
+})
 
 test("Create / Get", async () => {
-  const connection = await setUpDb()
+  const connection = getConnection()
   const repo = new AccountRepository(connection)
-
   const id = generateId()
   const type = AccountType.ADMIN
   const email = faker.internet.email()
@@ -24,6 +41,4 @@ test("Create / Get", async () => {
   expect(created.getEmail()).toBe(email)
   expect(created.getName().getFirst()).toBe(firstName)
   expect(created.getName().getLast()).toBe(lastName)
-
-  await connection.close()
 })
