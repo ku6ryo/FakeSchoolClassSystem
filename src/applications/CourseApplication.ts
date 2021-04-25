fef7a352aecb5a9ede7faa52116cbc3e473e42c2
ku6ryo <ku6ryo@gmail.com>
2021-04-25 14:53:58 +0900

    Added course creation request

--- src/applications/CourseApplication.ts
+++ src/applications/CourseApplication.ts
@@ -1,9 +1,13 @@
 import { PermissionError, } from "./errors"
 import Account from "../models/Account.model"
+import AccountType from "../models/AccountType"
+import Course from "../models/Course.model"
+import CourseCreationRequest from "../models/CourseCreationRequest.model"
 import CourseRepository, {
   CourseSearchResult,
 } from "../repositories/CourseRepositoryInterface"
 import PermissionService from "../services/PermissionService"
+import generateId from "../generators/id"
 
 export default class CourseApplication {
   #courseRepository: CourseRepository
@@ -15,6 +19,23 @@ export default class CourseApplication {
   ) {
     this.#courseRepository = courseRepository
     this.#permissionService = permissionService
+  }
+
+  createCourse (account: Account, req: CourseCreationRequest): Course {
+    if (account.getType() !== AccountType.ADMIN) {
+      throw new Error("Requester is not an admin.")
+    }
+    const id = generateId()
+    const course = new Course(
+      id,
+      req.getDisplayId(),
+      req.getCategory(),
+      req.getName(),
+      req.getDescription(),
+      req.getCredits(),
+    )
+    this.#courseRepository.create(course)
+    return course
   }
 
   searchCourses (
